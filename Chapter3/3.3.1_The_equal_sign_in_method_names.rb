60f85b4f9df0e09586177867225b6fef68562346
Alex Rogna <alexrogna@yahoo.com>
2020-12-01 16:06:36 +0000

    when methond name end in =, the call might be without parantheses

--- Chapter3/3.3.1_The_equal_sign_in_method_names.rb
+++ Chapter3/3.3.1_The_equal_sign_in_method_names.rb
@@ -14,7 +14,7 @@ class Ticket
 end
 
 ticket = Ticket.new("Town Hall", "2013-11-12")
-ticket.price=(63.00)
+ticket.price = 63.00
 puts "The ticket costs $#{"%.2f" % ticket.price}."
-ticket.price=(72.5)
+ticket.price = 72.5
 puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price} "
