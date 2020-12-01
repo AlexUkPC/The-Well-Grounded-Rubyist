4f519f80993a9002f75288589e038e4c29aa24a1
Alex Rogna <alexrogna@yahoo.com>
2020-12-01 14:26:00 +0000

    Exercise

---
+++ Chapter2/2.4.4_Exercise.rb
@@ -0,0 +1,24 @@
+ticket = Object.new
+def ticket.date
+  "1903-01-02"
+end
+def ticket.venue
+  "Town Hall"
+end
+def ticket.event
+  "Author's reding"
+end
+def ticket.performer
+  "Mark Twain"
+end
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+def ticket.price
+  5.50
+end
+def ticket.print_details(*x)
+  x.each{|detail| puts "This ticket is #{detail}"}
+end
+
+ticket.print_details("non-refundable","non-transferable","in a non smoking section")
