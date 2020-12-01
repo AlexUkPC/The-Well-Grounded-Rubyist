c53dd140e9c448a1803c6e81938326b05fccbdaf
Alex Rogna <alexrogna@yahoo.com>
2020-12-01 12:45:17 +0000

    Methods that take arguments

--- Chapter2/2.1.3_Methods_that_take_arguments.rb
+++ Chapter2/2.1.3_Methods_that_take_arguments.rb
@@ -4,7 +4,9 @@ def obj.c2f(c) # def obj.c2f c - parantheses can be omitted
 end
 puts obj.c2f(100)
 puts obj.c2f 0 # parantheses can be omitted
+
 def welcome_the_user # def welcome_the_user()- ussualy if no paramethers then parantheses are ommited
-    puts "Hi there!"
+  puts "Hi there!"
 end
+
 welcome_the_user
