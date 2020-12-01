111660d479fa0f48dcaa755f38d12e2a3d1e775a
Alex Rogna <alexrogna@yahoo.com>
2020-12-01 17:47:26 +0000

    Added 2 tests, one with another class method and one with a simple method

--- Chapter3/3.6.3_A_singleton_method_by_any_other_name.rb
+++ Chapter3/3.6.3_A_singleton_method_by_any_other_name.rb
@@ -11,6 +11,16 @@ class Ticket
   end
 end
 
+class TTT
+  def TTT.most_expensive_2(*tickets)
+    tickets.max_by(&:price)
+  end
+end
+
+def most_expensive_3(*tickets)
+  tickets.max_by(&:price)
+end
+
 th = Ticket.new("Town Hall", "2013-11-12")
 cc = Ticket.new("Convention Center", "2014-12-13")
 fg = Ticket.new("Fairgrounds", "2015-10-11")
@@ -19,4 +29,10 @@ cc.price=10
 fg.price=18
 highest= Ticket.most_expensive(th,cc,fg)
 puts "The highest-priced ticket is the one for #{highest.venue}."
+
+highest2 = TTT.most_expensive_2(th,cc,fg)
+puts "The highest-priced ticket with another class method #{highest2.venue}." #! it's working the same and I don't understand yet why
+
+highest3 = most_expensive_3(th,cc,fg)
+puts "The highest-priced ticket with a simple method #{highest3.venue}." #! it's working the same and I don't understand yet why
 
