6ddf693981bb0f219c2f2d00d744a6e1526c7cfc
Alex Rogna <alexrogna@yahoo.com>
2020-12-03 17:55:50 +0000

    @@total_count replaced by instance variable @total_count

--- Chapter5/5.2.5_Class_variable_syntax,scope_and_visibility.rb
+++ Chapter5/5.2.5_Class_variable_syntax,scope_and_visibility.rb
@@ -1,10 +1,13 @@
 class Car
   @@makes = []
   @@cars = {}
-  @@total_count = 0
+
   attr_reader :make
   def self.total_count
-    @@total_count
+    @total_count ||= 0
+  end
+  def self.total_count=(n)
+    @total_count = n
   end
   def self.add_make(make)
     unless @@makes.include?(make)
@@ -18,20 +21,22 @@ class Car
       puts "Creating a new #{make}!"
       @make = make
       @@cars[make] += 1
-      @@total_count += 1
-      else
-        raise "No such make: #{make}."
+      self.class.total_count += 1
+    else
+      raise "No such make: #{make}."
     end
   end
+
   def make_mates
     @@cars[self.make]
   end
 end
+
 Car.add_make("Honda")
 Car.add_make("Ford")
-h=Car.new("Honda")
-f=Car.new("Ford")
-h2=Car.new("Honda")
+h = Car.new("Honda")
+f = Car.new("Ford")
+h2 = Car.new("Honda")
 puts "Counting cars of same make as h2..."
 puts "There are #{h2.make_mates}."
 puts "Counting total cars..."
