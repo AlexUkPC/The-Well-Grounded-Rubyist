e73deed5fb55098e8fedd1fc797df4e537decbea
Alex Rogna <alexrogna@yahoo.com>
2020-12-06 17:52:35 +0000

    Role-playing to_star methods

---
+++ Chapter7/7.4.4_Role-playing_to_star_methods.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_accessor :name
+
+  def to_str
+    name
+  end
+end
+
+david = Person.new
+david.name = "David"
+puts "david is named " + david + "."
+
+class Person2
+  attr_accessor :name, :age, :email
+
+  def to_ary
+    [name, age, email]
+  end
+end
+
+david2=Person2.new
+david2.name = "David"
+david2.age = 55
+david2.email= "david@wherever"
+array = []
+array.concat(david2)
+p array
