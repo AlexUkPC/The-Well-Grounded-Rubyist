4c3c48260921b68cd84edf17961fa7d10c14cac3
Alex Rogna <alexrogna@yahoo.com>
2020-12-07 18:41:40 +0000

    String conversion

---
+++ Chapter8/8.1.6_String_conversion.rb
@@ -0,0 +1,11 @@
+puts "100".to_i(17) #=>289
+puts "100".oct #=>64
+puts "100".hex #=>256
+
+puts "1.2345".to_f #=>1.2345
+puts "Hello".to_str #=>Hello
+puts "-4e-2i".to_c #=>0-0.04i
+puts "4.55".to_r #=>91/20
+puts "abcde".to_sym #=>:abcde
+puts "1.2345and some words".to_f #=>1.2345
+puts "just some words".to_i #=>0
