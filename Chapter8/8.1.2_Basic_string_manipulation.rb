d9d7ab36a372d79af568c04ddd466e7c772c0ebb
Alex Rogna <alexrogna@yahoo.com>
2020-12-07 14:48:12 +0000

    Updated with outputs

--- Chapter8/8.1.2_Basic_string_manipulation.rb
+++ Chapter8/8.1.2_Basic_string_manipulation.rb
@@ -1,34 +1,34 @@
 string = "Ruby is a cool language."
-puts string[5]
-puts string[-12]
-puts string[5, 10]
-puts string[7...14]
-puts string[-12..-3]
-puts string[-12..20]
-puts string[15...-1]
-puts string["cool lang"]
+puts string[5] #=>i
+puts string[-12] #=>o
+puts string[5, 10] #=>is a cool
+puts string[7...14] #=> a cool
+puts string[-12..-3] #=>ol languag
+puts string[-12..20] #=>ol langua
+puts string[15...-1] #=>language
+puts string["cool lang"] #=>cool lang
 puts string["very cool lang"] #=>nil
-puts string[/c[ol ]+/]
+puts string[/c[ol ]+/] #=>cool l
 string.slice!("cool ")
-puts string
+puts string #=>Ruby is a language.
 
 string = "Ruby is a cool language."
 string["cool"] = "great"
-puts string
+puts string #=>Ruby is a great language.
 string[-1] = "!"
-puts string
+puts string #=>Ruby is a great language!
 string[-9..-1] = "thing to learn!"
-puts string
+puts string #=>Ruby is a great thing to learn!
 
 str = "Hi "
-puts str
-puts str + "there."
-puts str
+puts str #=>Hi
+puts str + "there." #=>Hi there.
+puts str #=>Hi
 
 str = "Hi "
-puts str
-puts str << "there."
-puts str
+puts str #=>Hi
+puts str << "there." #=>Hi there.
+puts str #=>Hi there.
 
 class Person
   attr_accessor :name
@@ -40,4 +40,4 @@ end
 
 david = Person.new
 david.name = "David"
-puts "Hello, #{david}!"
+puts "Hello, #{david}!" #=>Hello, David!
