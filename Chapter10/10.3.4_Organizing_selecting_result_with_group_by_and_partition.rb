f08452a8d9c9ab04e09a7b8b26d64fbf0d398fce
Alex Rogna <alexrogna@yahoo.com>
2020-12-10 20:12:37 +0000

    Organizing selecting result with group_by an partition

---
+++ Chapter10/10.3.4_Organizing_selecting_result_with_group_by_and_partition.rb
@@ -0,0 +1,18 @@
+colors = %w(red orange yello green blue indigo violet)
+p colors.group_by { |color| color.size }
+
+class Person
+  attr_accessor :age
+
+  def initialize(options)
+    self.age = options[:age]
+  end
+
+  def teenager?
+    (13..19) === age
+  end
+end
+
+people = 10.step(25, 3).map { |i| Person.new(:age => i) }
+p teens = people.partition { |person| person.teenager? }
+puts "#{teens[0].size} teens; #{teens[1].size} non-teens"
